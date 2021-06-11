5a202d7a4cc66cc1ef7b6b514503673ce85f6a2b
SrNightmare09 <srnightmare09@gmail.com>
2021-06-11 10:13:20 +0530

    Make card

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
 #!/usr/bin/env node
 
-console.log(`
-\n
-SrNightmare09
-Twitter: https://twitter.com/SrNightmare09
-`)
+"use strict";
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "What you want to do?",
+    choices: [
+      {
+        name: `Contact me through ${chalk.green.bold("email")}?`,
+        value: () => {
+          open("mailto:srnightmare09@gmail.com");
+          console.log("\nDone, see you soon.\n");
+        }
+      },
+      {
+        name: "Just quit.",
+        value: () => {
+          console.log("Bye, enjoy your day :).\n");
+        }
+      }
+    ]
+  }
+];
+
+const data = {
+  name: chalk.bold.green("        SrNightmare09"),
+  handle: chalk.white("@srnightmare09"),
+  blog: chalk.gray("https://medium.com/") + chalk.whiteBright("@srnightmare09"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("srnightmare09"),
+  npm: chalk.gray("https://npmjs.com/") + chalk.red("~srnightmare09"),
+  github: chalk.gray("https://github.com/") + chalk.green("srnightmare09"),
+  npx: chalk.red("npx") + " " + chalk.white("srnightmare09"),
+
+  labelBlog: chalk.white.bold("     Medium:"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelnpm: chalk.white.bold("        npm:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+  [
+    `${data.name} / ${data.handle}`,
+    ``,
+    `${data.labelBlog}  ${data.blog}`,
+    `${data.labelTwitter}  ${data.twitter}`,
+    `${data.labelnpm}  ${data.npm}`,
+    `${data.labelGitHub}  ${data.github}`,
+    ``,
+    `${data.labelCard}  ${data.npx}`,
+    ``,
+    `${chalk.italic(
+      "A very passionate and self-taught programmer."
+    )}`,
+    `${chalk.italic("Errors don't stop me from developing my ideas.")}`
+  ].join("\n"),
+  {
+    margin: 1,
+    float: 'center',
+    padding: 1,
+    borderStyle: "single",
+    borderColor: "cyan"
+  }
+);
+
+console.log(me);
+const tip = [
+  `Tip: ${chalk.greenBright.bold(
+    "cmd/ctrl + click"
+  )} to visit the links above`,
+  '',
+].join("\n");
+console.log(tip);
+
+
+prompt(questions).then(answer => answer.action());
